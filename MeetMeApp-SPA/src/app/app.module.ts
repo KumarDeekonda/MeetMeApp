912516987cb8b11bb7f1c043c5a7374c8f94f94e
KumarDeekonda <kumardvplr@gmail.com>
2018-08-27 01:10:04 -0400

    Nav, login, register and cancel componets added

--- MeetMeApp-SPA/src/app/app.module.ts
+++ MeetMeApp-SPA/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
+      NavComponent,
+      RegisterComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
       HttpModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [HttpClient,  ],
+   providers: [
+      HttpClient,
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
--- MeetMeApp.API/Controllers/ValuesController.cs
+++ MeetMeApp.API/Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace MeetMeApp.API.Controllers
             _context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
